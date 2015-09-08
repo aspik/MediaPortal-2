3a5a06aefb6295aeb4518c894c0607313591e576
morpheusxx <morpheus_xx@team-mediaportal.com>
2015-09-08 21:34:19 +0200

    Added "IsPlayerActive" to home model. Once a player gets active, the group menu is rebuilt and another button is added. If the button is pressed, the IsPlayerActive property will be set to true.

--- MediaPortal/Incubator/BlueVision/Models/HomeMenuModel.cs
+++ MediaPortal/Incubator/BlueVision/Models/HomeMenuModel.cs
@@ -30,6 +30,7 @@ using MediaPortal.Common;
 using MediaPortal.Common.Commands;
 using MediaPortal.Common.General;
 using MediaPortal.Common.Logging;
+using MediaPortal.Common.Messaging;
 using MediaPortal.Common.Services.Settings;
 using MediaPortal.Common.Settings;
 using MediaPortal.UI.Presentation.DataObjects;
@@ -38,6 +39,7 @@ using MediaPortal.UI.Presentation.Workflow;
 using MediaPortal.UiComponents.BlueVision.Settings;
 using MediaPortal.UiComponents.SkinBase.General;
 using MediaPortal.UiComponents.SkinBase.Models;
+using MediaPortal.UI.Presentation.Players;
 using MediaPortal.UI.SkinEngine.MpfElements;
 using MediaPortal.Utilities.Xml;
 
@@ -62,7 +64,9 @@ namespace MediaPortal.UiComponents.BlueVision.Models
     protected AbstractProperty _lastSelectedItemProperty;
     protected AbstractProperty _lastSelectedItemNameProperty;
     protected AbstractProperty _isHomeProperty;
+    protected AbstractProperty _isPlayerActiveProperty;
     protected bool _noSettingsRefresh;
+    protected bool _isPlayerActive;
 
     #endregion
 
@@ -183,6 +187,17 @@ namespace MediaPortal.UiComponents.BlueVision.Models
       set { _isHomeProperty.SetValue(value); }
     }
 
+    public AbstractProperty IsPlayerActiveProperty
+    {
+      get { return _isPlayerActiveProperty; }
+    }
+
+    public bool IsPlayerActive
+    {
+      get { return (bool)_isPlayerActiveProperty.GetValue(); }
+      set { _isPlayerActiveProperty.SetValue(value); }
+    }
+
     #endregion
 
     public HomeMenuModel()
@@ -190,7 +205,10 @@ namespace MediaPortal.UiComponents.BlueVision.Models
       _lastSelectedItemProperty = new WProperty(typeof(ListItem), null);
       _lastSelectedItemNameProperty = new WProperty(typeof(string), null);
       _isHomeProperty = new WProperty(typeof(bool), false);
+      _isPlayerActiveProperty = new WProperty(typeof(bool), false);
       IsHomeProperty.Attach(IsHomeChanged);
+
+      SubscribeToMessages();
 
       ReadPositions();
 
@@ -271,6 +289,16 @@ namespace MediaPortal.UiComponents.BlueVision.Models
             }
             _mainMenuGroupList.Add(groupItem);
           }
+          // "Currently playing button" if any player is active
+          if (_isPlayerActive)
+          {
+            var groupItem = new GroupMenuListItem(Consts.KEY_NAME, MenuSettings.MENU_NAME_PLAYING);
+            if (_menuSettings.Settings.DisableAutoSelection)
+              groupItem.Command = new MethodDelegateCommand(() => IsPlayerActive = true );
+
+            groupItem.AdditionalProperties["Id"] = MenuSettings.MENU_ID_PLAYING;
+            _mainMenuGroupList.Add(groupItem);
+          }
         }
       }
       _mainMenuGroupList.FireChange();
@@ -343,6 +371,14 @@ namespace MediaPortal.UiComponents.BlueVision.Models
 
     private void SetGroup(string groupId)
     {
+      bool wasPlayerActive = IsPlayerActive;
+      if (wasPlayerActive)
+      {
+        IsPlayerActive = false;
+        // Do not navigate in workflow, but only toggle visibility
+        return;
+      }
+
       if (_menuSettings.Settings.DefaultMenuGroupId == groupId)
         return;
       _menuSettings.Settings.DefaultMenuGroupId = groupId;
@@ -357,7 +393,7 @@ namespace MediaPortal.UiComponents.BlueVision.Models
           UpdateSelectedGroup();
         }
       }
-      finally 
+      finally
       {
         _noSettingsRefresh = false;
       }
@@ -484,6 +520,38 @@ namespace MediaPortal.UiComponents.BlueVision.Models
         _menuSettings.Settings.MainMenuGroupNames.Add(new GroupItemSetting { Name = MenuSettings.MENU_NAME_OTHERS, Id = new Guid(MenuSettings.MENU_ID_OTHERS) });
         ServiceRegistration.Get<ISettingsManager>().Save(menuSettings);
       }
+    }
+
+    private void SubscribeToMessages()
+    {
+      if (_messageQueue == null)
+        return;
+      _messageQueue.SubscribeToMessageChannel(PlayerManagerMessaging.CHANNEL);
+      _messageQueue.MessageReceived += OnMessageReceived;
+    }
+
+    private void OnMessageReceived(AsynchronousMessageQueue queue, SystemMessage message)
+    {
+      if (message.ChannelName == PlayerManagerMessaging.CHANNEL)
+      {
+        // React to player changes
+        PlayerManagerMessaging.MessageType messageType = (PlayerManagerMessaging.MessageType)message.MessageType;
+        switch (messageType)
+        {
+          case PlayerManagerMessaging.MessageType.PlayerEnded:
+          case PlayerManagerMessaging.MessageType.PlayerStopped:
+          case PlayerManagerMessaging.MessageType.PlayerStarted:
+            UpdatePlayerState();
+            break;
+        }
+      }
+    }
+
+    private void UpdatePlayerState()
+    {
+      _isPlayerActive = ServiceRegistration.Get<IPlayerContextManager>().PlayerContexts.Any(pc => pc.IsActive);
+      CreateMenuGroupItems();
+      CreatePositionedItems();
     }
   }
 }
